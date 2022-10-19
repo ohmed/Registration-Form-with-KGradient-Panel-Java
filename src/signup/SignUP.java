dc8bc40c1b6f14210c4cc3f4b22c77d9619eb987
AnonyMyke <87503468+AnonyMyke@users.noreply.github.com>
2022-10-20 00:52:27 +0300

    Update SignUP.java

--- src/signup/SignUP.java
+++ src/signup/SignUP.java
@@ -8,6 +8,7 @@ import javax.swing.JOptionPane;
 /**
  *
  * @author mike-oduor
+
  */
 public class SignUP extends javax.swing.JFrame {
 
@@ -306,6 +307,7 @@ public class SignUP extends javax.swing.JFrame {
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
